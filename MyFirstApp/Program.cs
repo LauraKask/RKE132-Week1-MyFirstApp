349768615f4b6e0de8e6c2938a029a4f3a5eb91e
Laura <laura.kask@tktk.ee>
2024-02-10 17:04:47 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+//rakenuds küsib kasutajalt sisestaded tema nime
+//rakendus tervitab kasutajate tema nime pidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne (andmetüüp, mis läheb kasti)
+string userName = Console.ReadLine();//Input
+
+Console.WriteLine("Tere" +", "+ userName + "!");//Output
+//string interpolations
+
+Console.WriteLine($"Hello, {userName} ? "); //Output
+
